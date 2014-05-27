975357fdb071d1e8bdbea337373f3863a0f2fac9
Kee Kee Mu <moongeegee@gmail.com>
2014-05-27 05:55:03 -0400

    Initial check in of HibernateSpringHibernateTemplateSaveOrUpdate

---
+++ HibernateSpringHibernateTemplateSaveOrUpdate/src/Main.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.HibernateCallback;
+
+import org.hibernate.*;
+import org.hibernate.criterion.*;
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		HibernateUtil.setup("create table EVENTS ( u_id int, name VARCHAR(60), start_Date date, duration int)");
+		
+		// hibernate code start
+        HibernateFactory.buildSessionFactory();
+        SessionFactory sessionFactory= HibernateFactory.getSessionFactory();
+        HibernateTemplate template= new HibernateTemplate(sessionFactory);
+
+
+        Event event = new Event();
+        event.setName("Spring Hibernate Template test1");
+        template.saveOrUpdate(event);
+        Event obj = (Event) template.load(Event.class, event.getId());
+
+        HibernateUtil.checkData("select name from events");        
+
+        HibernateFactory.closeFactory();
+        
+		// hibernate code end
+	}
+	
+}
